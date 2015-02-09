21e14b693a0144792c398922df3b6facb2d542fb
Serena Chan <chicoupons@gmail.com>
2015-02-09 02:47:50 +0000

    Updates

--- 01-recursion/Driver.java
+++ 01-recursion/Driver.java
@@ -30,14 +30,37 @@ public class Driver {
 
 
     public int strCount(String str, String sub) {
-	if (str.length() < sub.length) {
-	    return 0;
+  		int len = sub.length();
+  		if (str.length() < len) return 0;
+  		if (str.substring(0, len).equals(sub)) {
+     		return 1 + strCount(str.substring(len), sub);
+  		}
+  		else {
+     		return strCount(str.substring(1), sub);
+  		}
 	}
-	if (str.equals(sub)) {
-	    return 1;
+	
+	public int sumDigits(int n) {
+  		if (n < 10) {
+  			return n;
+  		}
+  		else {
+  			return (sumDigits(n / 10) + sumDigits(n % 10));
+  		}
 	}
-	else {
+	
+	public String allStar(String str) {
+  		if (str.length() < 2) return str;
+  		return str.charAt(0) + "*" + (allStar(str.substring(1)));
 	}
-    }
+
+	public static void main(String[] args) {
+		Driver d = new Driver();
+		System.out.println(d.fibonacci(5));
+		System.out.println(d.bunnyEars2(7));
+		System.out.println(d.strCount("qweabcdewabc", "abc"));
+		System.out.println(d.sumDigits(23958));
+		System.out.println(d.allStar("hello"));
+	}
 
 }
