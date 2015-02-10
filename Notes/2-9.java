d6b283936408073b7035bff0bb722c47341a4af5
serenadipity <serenagchan@gmail.com>
2015-02-10 10:59:57 -0500

    Updated

---
+++ Notes/2-9.java
@@ -0,0 +1,7 @@
+/*
+solve (x,y) --> base cases: 1. if @ exit    2. if at a wall
+                solve (x+1,y) otherwise
+		solve (x-1,y) ""   ""
+		solve (x,y+1) ""   ""
+		solve (x,y-1) ""   ""
+*/
