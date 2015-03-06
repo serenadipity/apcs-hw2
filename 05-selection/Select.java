b60ffe1bcd0ca0a3cc54fd64af15c219134d12f6
Serena Chan <serenagchan@gmail.com>
2015-03-05 21:52:27 -0500

    Compiles but prints out duplicates for some reason. Cannot find the error.

--- 05-selection/Select.java
+++ 05-selection/Select.java
@@ -11,9 +11,37 @@ import java.util.*;
 
 public class Select {
 
-    public Select(int kth, ArrayList<int> L, int size) {
+    public int[] pivot (int[] L, int SI, int EI) {
+	int[] D = new int[L.length];
+	for (int i=0; i < L.length; i++) {
+		D[i] = L[i];
+	}
+	int p = L[SI];
+	D[SI] = p;
+	for (int k = SI+1; k < EI; k++) {
+	    if (L[k] < p) {
+		D[SI]=L[k];
+		SI++;		
+	    }
+	    else {
+		D[EI]=L[k];
+		EI--;
+	    }	 
+	}
+	return D;
+    }
+
+    public String toString(int[] D) {
+	String s = "";
+	for (int i=0; i < D.length;i++) {
+	    s+=D[i] + ", ";
+	}
+	return s;
     }
 
     public static void main(String[] args) {
+	Select s = new Select();
+	int[] L = {1, 2, 5, 7, 8, 9, 12, 25, 56, 73};
+	System.out.println(s.toString(s.pivot(L,2,8)));
     }
 }
