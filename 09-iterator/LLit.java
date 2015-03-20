9c2408549ca181774270e42498fefae09d6063d2
serenadipity <serenagchan@gmail.com>
2015-03-20 09:25:40 -0400

    Updated

--- 09-iterator/LLit.java
+++ 09-iterator/LLit.java
@@ -2,23 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
-		private Node<E> t;
-		public LLit(Node<E> n){
-				t=n;
-		}
+    private Node<E> t;
+    public LLit(Node<E> n){
+	t=n;
+    }
 
-		public boolean hasNext(){
-				return t!=null;
-		}
+    public boolean hasNext(){
+	return t!=null;
+    }
 
-		public E next(){
-				E retval = t.getData();
-				t=t.getNext();
-				return retval;
-		}
+    public E next(){
+	E retval = t.getData();
+	t=t.getNext();
+	return retval;
+    }
 
-		public void remove(){
-				// to be completed for homework
-		}
+    public void remove(){
+	// to be completed for homework
+
+    }
 }
 
