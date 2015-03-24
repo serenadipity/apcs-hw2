6eb19637c3450fd737c74d341a01ccd06ee28d26
Serena Chan <serenagchan@gmail.com>
2015-03-24 00:06:51 -0400

    A bit confused with the assignment

--- 10-stacks/myStack.java
+++ 10-stacks/myStack.java
@@ -3,28 +3,37 @@ public class myStack<E>{
 
     // make whatever constructor(s) you need
 
-    private Node<E> l, dummy;
+    private Node<E> l;
+
+    private Node top, bottom;
 
     public myStack(E s) {
 	l = new Node(s);
+	top = null;
+	bottom = null;
     }
 
     public void push(E data){
         // add something to the top
-	Node n = new Node(data);
-	n.setNext(l);
+	Node<E> tmp = new Node<E>(data);
+        tmp.setNext(l.getNext());
+	l.setNext(tmp);
+	top = tmp;
     }
 
     public E pop(){
         // remove and return the top item from the stack
-	dummy.setNext(dummy.getNext().getNext());
+	Node tmp = top;
+        top.getPrev().setNext(null);
+	return tmp;
     }
 
     public boolean empty(){
-
+	if (l = null) return true;
     }
 
     public E top() {
         // return the top item from the stack
+	return top.getData();
     }
 }
