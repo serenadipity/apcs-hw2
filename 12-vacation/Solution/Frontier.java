63ab9c897b8c2ce6da3e52ccb1e48280eec96364
serenadipity <serenagchan@gmail.com>
2015-04-13 09:13:25 -0400

    Solutions to this assignment

---
+++ 12-vacation/Solution/Frontier.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Frontier {
+
+    public LinkedList<Node> l = new LinkedList<Node>();
+
+}
---
+++ 12-vacation/Solution/Maze.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.util.*;
+
+public class Maze 
+{
+    private char[][] board;
+    private int maxX;
+    private int maxY;
+
+    private char path='#';
+    private char wall=' ';
+    private char me='z';
+    private char exit='$';
+    private char visited = '.';
+    private boolean solved = false;
+		
+    public void delay(int n){
+	try {
+	    Thread.sleep(n);
+	} catch (Exception e) {}
+    }
+		
+    public Maze() 
+    {
+	maxX=40;
+	maxY=20;
+	board = new char[maxX][maxY];
+				
+	try {
+						
+	    Scanner sc = new Scanner(new File("maze.dat"));
+	    int j=0;
+	    while (sc.hasNext())
+		{
+		    String line = sc.nextLine();
+		    for (int i=0;i<maxX;i++)
+			{
+			    board[i][j] = line.charAt(i);
+			}
+		    j++;
+		}
+	}
+	catch (Exception e)
+	    {
+	    }
+				
+    }
+		
+    public String toString()
+    {
+	String s = "[2J\n";
+				
+	for (int y=0;y<maxY;y++)
+	    {
+		for (int x=0;x<maxX;x++)
+		    s = s +board[x][y];
+		s=s+"\n";
+	    }
+	return s;
+    }
+
+    /*
+      solved - instance variable to indicate we're done
+			
+    */
+    public void solve(int x, int y){
+	if (board[x][y]==wall ||
+	    board[x][y]==me ||
+	    board[x][y]==visited ||
+	    solved){
+	    return;
+	}
+
+	if (board[x][y]==exit){
+	    System.out.println(this);
+	    solved = true;
+	}
+	delay(100);
+	System.out.println(this);
+	board[x][y]=me;
+	solve(x+1,y);
+	solve(x-1,y);
+	solve(x,y+1);
+	solve(x,y-1);
+	board[x][y]=visited;
+    }
+
+    public void addToFront(int tx, int ty) {
+	if (board[tx][ty]=='#' || 
+
+    public void bfs(int x, int y) {
+	Frontier f = new Frontier();
+	f.add(new Node(x,y));
+
+	Node tmp = null;
+	int tx = 0;
+	int ty = 0;
+
+	while (!f.isEmpty()) {
+	    Node current = f.remove();
+	    int cx = current.getX();
+	    int cy = current.getY();
+
+	    board[x][y] = 'z';
+
+	    addToFront(cx+1,cy);
+	    addToFront(cx-1,cy);
+	    addToFront(cx,cy+1);
+	    addToFront(cx,cy-1);
+
+	    delay(50);
+	    System.out.println(this);
+	}
+    }
+	    
+		
+    public static void main(String[] args){
+	Maze m = new Maze();
+	System.out.println(m);
+	m.solve(1,1);
+    }
+}
