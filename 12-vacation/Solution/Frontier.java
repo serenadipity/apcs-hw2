d4c839570129e31dc5aeeff42561f763c42b7795
serenadipity <serenagchan@gmail.com>
2015-04-14 09:11:57 -0400

    Notes

--- 12-vacation/Solution/Frontier.java
+++ 12-vacation/Solution/Frontier.java
@@ -4,4 +4,15 @@ public class Frontier {
 
     public LinkedList<Node> l = new LinkedList<Node>();
 
+    public void add(Node n) {
+	l.add(n);
+    }
+
+    public Node remove(Node n) {
+	Node tmp = l.remove();
+	return tmp;
+    }
+
+
+
 }
---
+++ Notes/4-14/Notes.java
@@ -0,0 +1,14 @@
+/*
+
+Heuristic Search
+
+Best first search
+    -> frontier is a priority Queue
+         ->things are removed based on priority
+           ex: insert in order based on priority and remove from b 
+	   or
+	   insert at end remove by searching for max/min priority
+
+Priority is a simple to compute value that you ??? gets you to the exit quickly
+
+*/
