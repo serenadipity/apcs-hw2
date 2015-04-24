b8a9e62015e8d569c8574b907a947f0c3c25f172
serenadipity <serenagchan@gmail.com>
2015-04-24 08:50:59 -0400

    Updated

--- 14-binary/Node.java
+++ 14-binary/Node.java
@@ -1,3 +1,38 @@
 public class Node {
 
+    public Node(){
+    }
+
+    public Node(int i){
+	data = i;
+    }
+
+    public int getData(){
+	return data;
+    }
+
+    public void setData(int x){
+	data = x;
+    }
+	
+
+    public Node getLeft(){
+	return left;
+    }
+
+    public void setLeft(Node n){
+	left = n;
+    }
+    
+    public Node getRight(){
+	return right;
+    }
+
+    public void setRight(Node n){
+	right = n;
+    }
+
+    public int compareTo(Node n){
+	return this.data-n.getData();
+    }
 }
