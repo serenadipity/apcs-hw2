02683539b1fb02b16d1c0d0792ffeea9489b87e6
serenadipity <serenagchan@gmail.com>
2015-05-11 09:26:20 -0400

    Updated

---
+++ 15-trees/Trees.java
@@ -0,0 +1,12 @@
+public class Trees {
+
+
+    public numNodes(Node T) {
+	if (T == null) {
+	    return 0;
+	}
+	else {
+	    return t.getVal() + numNodes(t.getLeft() + numNodes(t.getRight()));
+	}
+    }
+}
